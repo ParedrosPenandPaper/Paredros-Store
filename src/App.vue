8d6ad1f2789502b6de377f301d35ba56c807df5e
MeisterFa <meisterfa68426@th-nuernberg.de>
2019-09-29 22:08:52 +0200

    Created google login Button without Functionality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <navigation></navigation>
-    <hr> <!--- Ismael wird dieses hr ausschimpfen --->
+    <hr> <!--- @Ismael wird dieses hr ausschimpfen --->
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
     <router-view />
   </div>
 </template>
@@ -13,8 +14,43 @@
     name: 'app',
     components: {
       navigation
+    },
+    methods : {
+        loadScript : function () {
+          // eslint-disable-next-line no-unused-vars
+          let metaClientID = document.createElement('meta')
+          metaClientID.setAttribute('name', 'google-signin-client_id')
+          metaClientID.content = '215890179836-p9l9tc3kbqpvm9ujt6i6id7u5k387svt.apps.googleusercontent.com'
+          document.head.appendChild(metaClientID)
+
+          let googleSignInScript = document.createElement('script')
+          googleSignInScript.setAttribute('src', 'https://apis.google.com/js/platform.js')
+          googleSignInScript.async = true
+          googleSignInScript.defer = true
+          document.head.appendChild(googleSignInScript)
+        },
+      onSignIn : function(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
+
+  },
+    mounted() {
+      this.loadScript()
     }
   }
+
+
 </script>
 
 <style>
